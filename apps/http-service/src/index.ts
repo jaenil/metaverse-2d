dfcfd1690ac411e22728e90cafc3b229146aeba5
jaenil <jaenilparekh@gmail.com>
2026-06-15 17:06:27 +0530

    feat: implement Zod validation for auth routes, update schema constraints, and configure database environment variables

--- apps/http-service/src/index.ts
+++ apps/http-service/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import {router} from './routes/v1/index.js';
-import prisma from '@repo/db' ;
+import { router } from './routes/v1/index.js';
+import prisma from '@repo/db';
 const app = express();
-
+app.use(express.json());
 app.get('/', (req, res) => {
-    res.send('Backend is running currently at time : ' + new Date().toLocaleString()) ;
+    res.send('Backend is running currently at time : ' + new Date().toLocaleString());
 });
 
 app.use('/api/v1', router);
 
-app.listen(process.env.PORT ||3000) 
+app.listen(process.env.PORT || 3000) 
